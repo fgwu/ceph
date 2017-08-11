4899b3e8f2f2d198b92e37435d0c56fc73537fa0
Fenggang Wu <fenggang.wu@gmail.com>
2017-08-11 15:58:31 -0500

    backup

--- rocksdb_scripts/bulk_load_random.sh
+++ rocksdb_scripts/bulk_load_random.sh
@@ -16,8 +16,9 @@ mb=1073741824;
 wbs=268435456; 
 dds=1; 
 sync=0; 
+#r=10000000; 
 r=10000000; 
-t=1; 
+t=16; 
 vs=800; 
 bs=65536; 
 cs=1048576; 
@@ -25,7 +26,10 @@ of=500000;
 si=1000000; 
 stat=0;
 
-../build/bin/db_bench --benchmarks=fillrandom --disable_seek_compaction=1 --mmap_read=0 --statistics=$stat --histogram=1 --num=$r --threads=$t --value_size=$vs --block_size=$bs --cache_size=$cs --bloom_bits=10 --cache_numshardbits=4 --open_files=$of --verify_checksum=1 --sync=$sync --disable_wal=1 --compression_type=zlib --stats_interval=$si --compression_ratio=0.5  --write_buffer_size=$wbs --target_file_size_base=$mb --max_write_buffer_number=$wbn --max_background_compactions=$mbc --level0_file_num_compaction_trigger=$ctrig --level0_slowdown_writes_trigger=$delay  --level0_stop_writes_trigger=$stop  --num_levels=$levels --delete_obsolete_files_period_micros=$del --min_level_to_compress=$mcz --stats_per_interval=1 --max_bytes_for_level_base=$bpl --memtablerep=vector --use_existing_db=0 --disable_auto_compactions=1 #--disable_data_sync=$dds # --max_grandparent_overlap_factor=$overlap --source_compaction_factor=10000000 # --db=/data/mysql/leveldb/test 
+#../build/bin/db_bench_blue --benchmarks=fillrandom --disable_seek_compaction=1 --mmap_read=0 --statistics=$stat --histogram=1 --num=$r --threads=$t --value_size=$vs --block_size=$bs --cache_size=$cs --bloom_bits=10 --cache_numshardbits=4 --open_files=$of --verify_checksum=1 --sync=$sync --disable_wal=1 --compression_type=zlib --stats_interval=$si --compression_ratio=0.5  --write_buffer_size=$wbs --target_file_size_base=$mb --max_write_buffer_number=$wbn --max_background_compactions=$mbc --level0_file_num_compaction_trigger=$ctrig --level0_slowdown_writes_trigger=$delay  --level0_stop_writes_trigger=$stop  --num_levels=$levels --delete_obsolete_files_period_micros=$del --min_level_to_compress=$mcz --stats_per_interval=1 --max_bytes_for_level_base=$bpl --memtablerep=vector --use_existing_db=0 --disable_auto_compactions=1 #--disable_data_sync=$dds # --max_grandparent_overlap_factor=$overlap --source_compaction_factor=10000000 # --db=/data/mysql/leveldb/test 
 
 
 #open error: Corruption: no meta-nextfile entry in descriptor
+
+
+../build/bin/db_bench_blue --benchmarks=fillrandom --disable_seek_compaction=1 --mmap_read=0 --statistics=$stat --histogram=1 --num=$r --threads=$t #--value_size=$vs --block_size=$bs --cache_size=$cs --bloom_bits=10 --cache_numshardbits=4 --open_files=$of --verify_checksum=1 --sync=$sync --disable_wal=1 --compression_type=zlib --stats_interval=$si --compression_ratio=0.5  --write_buffer_size=$wbs --target_file_size_base=$mb --max_write_buffer_number=$wbn --max_background_compactions=$mbc --level0_file_num_compaction_trigger=$ctrig --level0_slowdown_writes_trigger=$delay  --level0_stop_writes_trigger=$stop  --num_levels=$levels --delete_obsolete_files_period_micros=$del --min_level_to_compress=$mcz --stats_per_interval=1 --max_bytes_for_level_base=$bpl --memtablerep=vector --use_existing_db=0 --disable_auto_compactions=1 #--disable_data_sync=$dds # --max_grandparent_overlap_factor=$overlap --source_compaction_factor=10000000 # --db=/data/mysql/leveldb/test 
